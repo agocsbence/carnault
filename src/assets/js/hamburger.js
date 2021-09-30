e6c6b8f6a30a21b4a07fd82fa760e394dfb6d23e
Bence Agócs <bence@harom.studio>
2021-09-30 15:34:33 +0200

    home page mobile done

--- src/assets/js/hamburger.js
+++ src/assets/js/hamburger.js
@@ -1,4 +1,8 @@
 var hamburger = document.querySelector(".hamburger");
+var nav = document.querySelector(".mobile-nav");
+var body = document.querySelector('body');
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
+    nav.classList.toggle('opened');
+    body.classList.toggle('lock-scroll');
 });
